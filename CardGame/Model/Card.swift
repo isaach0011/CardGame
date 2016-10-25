533fac0ac81ae585c67232743831f355e4d79007
isaach0011 <ihil17523@740s-pl-177.local>
2016-10-25 13:15:35 -0600

    Added Card classes to Xcode.
    Organized my groups in Xcode as well.

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Hill, Isaac on 10/25/16.
+//  Copyright © 2016 Hill, Isaac. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Hill, Isaac on 10/25/16.
+//  Copyright © 2016 Hill, Isaac. All rights reserved.
+//
+
+import Foundation
