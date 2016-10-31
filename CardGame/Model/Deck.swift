807cd3d6d4624d371d61cda76a3f546499ddd94a
isaach0011 <ihil17523@740s-pl-177.local>
2016-10-31 12:33:51 -0600

    Added deck and subclass playingcarddeck.

---
+++ CardGame/Model/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Hill, Isaac on 10/31/16.
+//  Copyright © 2016 Hill, Isaac. All rights reserved.
+//
+
+import Foundation
+
+class Deck
+{
+    
+}
