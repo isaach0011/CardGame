de56b82448ac256fedf1d6994463357ebb86c97d
isaach0011 <ihil17523@740s-pl-177.local>
2016-11-09 12:07:21 -0700

    Created the shuffleDeck method.

--- CardGame/Model/Deck.swift
+++ CardGame/Model/Deck.swift
@@ -14,7 +14,25 @@ class Deck
     
     func shuffleDeck() -> Void
     {
+        //Create a temp variable to hold Cards.
+        var tempDeck = [Card]()
         
+        //Repeat until cards ca member is empty
+        while self.cards.count > 0
+        {
+            //Get a reandom number between 0....cards.count-1
+            let randomIndex = Int (arc4random() % (UInt32)(cards.count))
+            
+            
+            //Remove the card at that inex from the deck.
+            let removedCard = cards.removeAtIndex(randomIndex)
+            
+            //Place that card in temporary array.
+            tempDeck.append(removedCard)
+        }
+        
+        //Replace the data member with the updated data.
+        self.cards = tempDeck
     }
     
     func cutDeck() -> Void
