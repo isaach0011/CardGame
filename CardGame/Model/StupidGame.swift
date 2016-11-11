dcb6097931eb000f89c33391a5e652b68485b0e7
isaach0011 <ihil17523@740s-pl-177.local>
2016-11-11 13:06:50 -0700

    Edited StupidGame and start declaring variables.

--- CardGame/Model/StupidGame.swift
+++ CardGame/Model/StupidGame.swift
@@ -6,9 +6,27 @@
 //  Copyright © 2016 Hill, Isaac. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class StupidGame
 {
-
+    internal var gameDeckOne : PlayingCardDeck
+    internal var gameDeckTwo : PlayingCardDeck
+    internal var gameCardOne : PlayingCard
+    internal var gameCardTwo : PlayingCard
+    internal var point = Int()
+    
+    init()
+    {
+        gameDeckOne = PlayingCardDeck()
+        gameDeckTwo = PlayingCardDeck()
+        gameCardOne = PlayingCard()
+        gameCardTwo = PlayingCard()
+        point = 0
+    }
+    
+    func startGame() -> Void
+    {
+        
+    }
 }
