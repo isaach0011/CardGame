2a0a56bc112df4150ba6b3adea5b8a15f34022ac
isaach0011 <ihil17523@740s-pl-2.local>
2016-11-21 13:13:00 -0700

    Got rid of all references of backImage.

--- CardGame/Model/Card.swift
+++ CardGame/Model/Card.swift
@@ -6,24 +6,17 @@
 //  Copyright © 2016 Hill, Isaac. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 class Card
 {
-    internal var backImage : UIImage
     internal var isFaceUp : Bool
     
     init()
     {
-        backImage = UIImage(named: "cardback")!
         isFaceUp = false
     }
-    
-    func getBackImage() -> UIImage
-    {
-        return backImage
-    }
-    
+
     func isFacing() -> Bool
     {
         return isFaceUp
@@ -34,13 +27,9 @@ class Card
         self.isFaceUp = isFaceUp
     }
     
-    func setBackImage(backImage : UIImage)
-    {
-        self.backImage = backImage
-    }
     func toString() -> String
     {
-        let description = "This card is \(isFaceUp) and \(self.backImage) is the image."
+        let description = "This card is \(isFaceUp)."
         
         return description
     }
