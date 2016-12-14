d3aefe5132d1fd1940ea913815cc204082e941a3
isaach0011 <ihil17523@740s-pl-121.local>
2016-12-14 13:55:49 -0700

    Started on creating an Auto Button.

--- CardGame/Controller/CardGameController.swift
+++ CardGame/Controller/CardGameController.swift
@@ -15,6 +15,10 @@ class CardGameController : UIViewController
     fileprivate var game = StupidGame()
     fileprivate var cardOne = String()
     fileprivate var cardTwo = String()
+    var timer: Timer!
+    var countdown: Int = 0
+    var previousPoints = 0
+    var k = 1
     
     
     @IBOutlet weak var pointName: UILabel!
@@ -26,11 +30,13 @@ class CardGameController : UIViewController
 
     override func viewDidLoad() -> Void
     {
-
+        self.countdown = 5
+        self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(CardGameController.updateCountdown)), userInfo: nil, repeats: true)
     }
     
     @IBAction func resetButton(_ sender: UIButton)
     {
+        //Resets the entire game.
         game.startGame()
         cardButtonOne.setTitle("Draw", for: UIControlState())
         cardButtonTwo.setTitle("Draw", for: UIControlState())
@@ -46,7 +52,57 @@ class CardGameController : UIViewController
         cardButtonTwo.setTitle(game.getCardTwo(), for: UIControlState())
     }
   
+    @IBAction func autoButton(_ sender: UIButton)
+    {
+       var previousPoints = 0
+       var k = 1
+       
+        while k == 1
+        {
+            point += game.playGame()
+            pointLabel.text = "\(point)"
+            print(point)
+            cardButtonOne.setTitle(game.getCardOne(), for: UIControlState())
+            cardButtonTwo.setTitle(game.getCardTwo(), for: UIControlState())
+            print("About to sleep")
+            sleep(1)
+            print("Sleepy")
+            if point == 52
+            {
+                k = 0
+                cardButtonOne.setTitle("Max Achieved", for: UIControlState())
+                cardButtonTwo.setTitle("Max Achieved", for: UIControlState())
+            }
+            else if previousPoints < point
+            {
+               previousPoints = point
+            }
+            else
+            {
+                //Resets points and decks.
+                game.startGame()
+                cardButtonOne.setTitle("Draw", for: UIControlState())
+                cardButtonTwo.setTitle("Draw", for: UIControlState())
+                point = 0
+                pointLabel.text = "\(point)"
+            }
+        
+            self.view.setNeedsDisplay()
+        }
+    }
     
+    func updateCountdown()
+    {
+        print("Time until launch \(self.countdown)")
+        
+        self.countdown -= 1
+        
+        if self.countdown == 0
+        {
+            self.timer.invalidate()
+            self.timer = nil
+        }
+    }
 //    @IBAction func cardClick(sender: UIButton)
 //    {
 //        clickCount += 1
